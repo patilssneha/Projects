3d3aacd6bca787416c8038d378f84d039efddeff
Snehal Patil <141490358+patilssneha@users.noreply.github.com>
2025-11-16 12:48:56 +0530

    Add files via upload

---
+++ Snehal.java
@@ -0,0 +1,22 @@
+public class Snehal {
+    public static void main (String args[]) {
+        //System.out.println("even numbers from 1 to 100:");
+        //for (int i = 1; i <= 100; i++) {
+
+            //if (i % 2 == 0) {
+               // System.out.println(i);
+           // }
+            //for (int i = 0; i < 5; i++) {
+                ///for( int j=0; j< 5; i++){
+               // System.out.println("*");
+int []
+                }
+
+            }
+
+    }
+
+
+
+
+
